8806b0b2a4bd8ad879fc1e4a7b1780e35c681ff1
pearsonben98 <pearsonben98@gmail.com>
2020-02-28 20:59:06 +0000

    working finger tapping plot

--- fingerPlot.m
+++ fingerPlot.m
@@ -1,10 +1,29 @@
 controlType = readtable('controlType.csv');
 parkinsonsType = readtable('parkinsonsType.csv');
 
-xCT = controlType(1:end, 2);
-yCT = controlType(1:end, 3);
-zCT = controlType(1:end, 4);
+frames = 300;
 
-xPT = parkinsonsType(1:end, 2);
-yPT = parkinsonsType(1:end, 3);
-zPT = parkinsonsType(1:end, 4);
+for i = 1 : frames
+    
+    x1CT = controlType{1*i:2*i,2};
+    x2CT = controlType{2*i:2*i,2};
+   
+    y1CT = controlType{1*i:2*i,3};
+    y2CT = controlType{2*i:2*i,3};
+    
+    z1CT = controlType{1*i:2*i,4};
+    z2CT = controlType{2*i:2*i,4};
+    
+    %plot3(xCT, yCT, zCT, 'o');
+    
+    plot3(x1CT, y1CT, z1CT, x2CT, y2CT, z2CT, 'o', 'LineWidth', 2.0);
+    plot3(x2CT, y2CT, z2CT, 'o', 'LineWidth', 2.0);
+
+    set(gcf, 'Position',  [25, 25, 1200, 1900]);
+    xlim([10,20]);
+    ylim([5,20]);
+    zlim([-4,2]);
+    
+    
+    pause(.05);
+end
